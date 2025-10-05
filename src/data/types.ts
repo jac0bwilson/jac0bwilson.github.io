37e1572ae17870758cb50d76ecbdfe474eb3684e
Jacob Wilson <jacobswilsonglos@gmail.com>
2025-10-05 23:00:54 +0100

    Add image loading

--- src/data/types.ts
+++ src/data/types.ts
@@ -24,3 +24,17 @@ export type Job = {
     location: string;
     description: string;
 };
+
+export type CloudinaryResource = {
+    public_id: string;
+    format: string;
+    version: number;
+    resource_type: string;
+    type: string;
+    created_at: string;
+    bytes: number;
+    width: number;
+    height: number;
+    url: string;
+    secure_url: string;
+};
