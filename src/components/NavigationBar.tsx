b1ccccfa143b35c9617bfdf3dbaab0e174778311
Jacob Wilson <jacobswilsonglos@gmail.com>
2025-10-05 22:34:02 +0100

    Add navigation for photography page

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,11 +1,55 @@
-import { Navbar, NavbarBrand } from '@heroui/react';
+import { useState } from 'react';
+import {
+    Link,
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    NavbarMenu,
+    NavbarMenuItem,
+    NavbarMenuToggle,
+} from '@heroui/react';
 
-export default function NavigationBar() {
+export default function NavigationBar({
+    currentPath,
+}: {
+    currentPath: string;
+}) {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const menuItems = [
+        { text: 'Home', href: '/' },
+        { text: 'Photography', href: '/photography/' },
+    ];
+
     return (
-        <Navbar>
+        <Navbar isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
             <NavbarBrand>
                 <p className="font-bold">Jacob Wilson</p>
             </NavbarBrand>
+            <NavbarContent className="hidden sm:flex">
+                {menuItems.map((item) => (
+                    <NavbarItem
+                        key={item.href}
+                        isActive={currentPath === item.href}
+                    >
+                        <Link href={item.href}>{item.text}</Link>
+                    </NavbarItem>
+                ))}
+            </NavbarContent>
+
+            <NavbarMenuToggle className="sm:hidden" />
+
+            <NavbarMenu>
+                {menuItems.map((item) => (
+                    <NavbarMenuItem
+                        key={item.href}
+                        isActive={currentPath === item.href}
+                    >
+                        <Link href={item.href}>{item.text}</Link>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
         </Navbar>
     );
 }
